c7fbb3e7112ee7a420ccbdfd9557f902495336d6
unknown <dothinhodut@gmail.com>
2023-04-13 14:42:52 +0700

    Database

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-const userRoute = require('./routes/user')
+// const userRoute = require('./routes/user')
 
-app.use(express.json())
-app.use('/user', userRoute);
+// app.use(express.json())
+// app.use('/user', userRoute);
 
-app.listen(port, function(){
-    console.log(`Your app running on port ${port}`);
-})
+// app.listen(port, function(){
+//     console.log(`Your app running on port ${port}`);
+// })
+
+
+
